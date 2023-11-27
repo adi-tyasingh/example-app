0045c582134067a1020e532a896a8e378f0f9293
aditya Singh <singhap_2@rknec.edu>
2023-11-27 23:17:43 +0530

    added Duration data field

--- database/migrations/2023_11_27_092928_create_songs_table.php
+++ database/migrations/2023_11_27_092928_create_songs_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->timestamps();
             $table->string('title');
             $table->string('artist');
+            $table->string('duration');
         });
     }
 
--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -16,21 +16,25 @@ class SongSeeder extends Seeder
         Song::create([
             'artist' => 'Eminem',
             'title' => 'Stan',
+            'duration'=>'5:32'
         ]);
 
         Song::create([
             'artist' => 'A P Dhillon',
             'title' => 'With You',
+            'duration'=>'3:34'
         ]);
 
         Song::create([
             'artist' => 'Led Zepplin',
             'title' => 'Stairway to Heaven',
+            'duration'=>'8:05'
         ]);
 
         Song::create([
             'artist' => 'Eagles',
             'title' => 'Hotel California',
+            'duration'=>'5:52'
         ]);
     }
 }
