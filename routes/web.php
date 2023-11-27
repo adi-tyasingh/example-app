0b95c6b99c30b59c01045179e0202360bb7b8ae0
aditya Singh <singhap_2@rknec.edu>
2023-11-27 14:55:42 +0530

    Render songs from Song objects

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Practicals\Song;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -30,6 +30,23 @@ Route::get('/veggies/{veggieName}', function ($veggieName) {
         abort(404);
     }
 });
+
+Route::get('/songs', function () {
+    $song1 = new Song();
+    $song1->setTitle("Stan");
+    $song1->setArtist("Eminem");
+  
+    $song2 = new Song();
+    $song2->setTitle("Nothing Else Matters");
+    $song2->setArtist("Metallica");
+  
+    $song3 = new Song();
+    $song3->setTitle("With You");
+    $song3->setArtist("A P Dhillon");
+  
+    return view('songs', [ 'songs' => [ $song1, $song2, $song3 ] ]); 
+  });
+
 
 Route::get('/veggies', function () {
     return view('veggies');
